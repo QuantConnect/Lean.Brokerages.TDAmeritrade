7c4263aa2bf155d7bc5b5ba5de3002c6c5e37358
Romazes <romazes20@gmail.com>
2022-10-07 00:14:08 +0300

    feature: add config.json

---
+++ QuantConnect.TDAmeritrade.Tests/TestSetup.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using QuantConnect.Configuration;
+using QuantConnect.Logging;
+using QuantConnect.Tests;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantConnect.TradingTechnologiesTests
+{
+    [TestFixture]
+    public class TestSetup
+    {
+        [Test, TestCaseSource(nameof(TestParameters))]
+        public void TestSetupCase()
+        {
+        }
+
+        public static void ReloadConfiguration()
+        {
+            // nunit 3 sets the current folder to a temp folder we need it to be the test bin output folder
+            var dir = TestContext.CurrentContext.TestDirectory;
+            Environment.CurrentDirectory = dir;
+            Directory.SetCurrentDirectory(dir);
+            // reload config from current path
+            Config.Reset();
+
+            var environment = Environment.GetEnvironmentVariables();
+            foreach (DictionaryEntry entry in environment)
+            {
+                var envKey = entry.Key.ToString();
+                var value = entry.Value.ToString();
+
+                if (envKey.StartsWith("QC_"))
+                {
+                    var key = envKey.Substring(3).Replace("_", "-").ToLower();
+
+                    Log.Trace($"TestSetup(): Updating config setting '{key}' from environment var '{envKey}'");
+                    Config.Set(key, value);
+                }
+            }
+
+            // resets the version among other things
+            Globals.Reset();
+
+            TestGlobals.Initialize();
+        }
+
+        private static void SetUp()
+        {
+            Log.LogHandler = new CompositeLogHandler();
+            Log.Trace("TestSetup(): starting...");
+            ReloadConfiguration();
+        }
+
+        private static TestCaseData[] TestParameters
+        {
+            get
+            {
+                SetUp();
+                return new[] { new TestCaseData() };
+            }
+        }
+    }
+}

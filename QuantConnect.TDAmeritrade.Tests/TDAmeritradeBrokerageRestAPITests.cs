a9e7f4425c04c7526d6d01bfa3d8aa7a3e921e52
Romazes <romazes20@gmail.com>
2022-12-12 16:19:27 +0200

    feature:  improve test GetAsk

--- QuantConnect.TDAmeritrade.Tests/TDAmeritradeBrokerageRestAPITests.cs
+++ QuantConnect.TDAmeritrade.Tests/TDAmeritradeBrokerageRestAPITests.cs
@@ -54,10 +54,17 @@ namespace QuantConnect.Tests.Brokerages.TDAmeritrade
 
         [TestCase(true, "LODE", "AAPL", "IBM")]
         [TestCase(false, "LODEE", "AAPLA", "IBMA")]
+        [TestCase(false, "")]
         public void GetAsk(bool expected, params string[] symbols)
         {
-            var quotes = _brokerage.GetQuotes(symbols);
-            Assert.That(quotes.Count() == symbols.Length, Is.EqualTo(expected));
+            try
+            {
+                var quotes = _brokerage.GetQuotes(symbols);
+                Assert.That(quotes.Count() == symbols.Length, Is.EqualTo(expected));
+            } catch (Exception exception)
+            {
+                Assert.IsTrue(exception is ArgumentException);
+            }
         }
     }
 }
